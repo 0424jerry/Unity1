a020c80c899ba0f6c85ac83cf1e57ca3100a038b
0424jerry <jerrygithub@gms.ndhu.edu.tw>
2020-10-27 11:33:23 +0800

    Rotate

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset = new  Vector3(0.23f, 5.44f, -10.93f);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
--- Assets/Scripts/PlayController.cs
+++ Assets/Scripts/PlayController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayController : MonoBehaviour
 {
+    public float speed = 1;
+    public float turnspeed = 10000000;
+    public float horizontalInput;
+    public float verticalInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,12 @@ public class PlayController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * 20); 
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        //transform.Translate(Vector3.right * Time.deltaTime * turnspeed * horizontalInput);
+        transform.Rotate(Vector3.up * Time.deltaTime * turnspeed * horizontalInput);
+        
+
     }
 }
